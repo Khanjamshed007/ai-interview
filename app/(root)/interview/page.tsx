be491fae58d15f0626654a79694ded4cc6149650
jamshed khan <khanjamshed404@gmail.com>
2025-05-02 20:41:15 +0530

    implement gemini and vapi

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,14 @@
+import Agent from '@/components/Agent'
+import React from 'react'
+
+const page = () => {
+    return (
+        <>
+            <h3>Interview Genration</h3>
+
+            <Agent userName="John Doe" type='interview' />
+        </>
+    )
+}
+
+export default page
